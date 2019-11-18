5d16f6cbb37b54578be2219937a1afd454295bfd
ep <ep@no.mail>
2019-11-18 16:07:53 +0200

    Pytest: Add a dummy test for pytest
    Add a dummy test for pytest in order to avoid a scenario that pytest
    will return an error due to 'no tests run'.
    
    Signed-off-by: ep <ep@no.mail>

---
+++ tests/dummy_pytest_test.py
@@ -0,0 +1,7 @@
+
+
+def test_dummy():
+    # A dummy test for pytest.
+    # Note: By default, pytest will run all functions that match the glob pattern test* in all files that matches any
+    #       of the globe patterns of test_*.py and *_test.py.
+    return 0  # always pass
